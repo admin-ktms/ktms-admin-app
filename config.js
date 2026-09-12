5388b6ab6344d876439103c5f13d8ffbd0af0559
admin-ktms <admin.ktms@gmail.com>
2026-09-12 10:28:00 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+export const CONFIG = {
+  SUPABASE_URL:
+    "https://gfokwfsqsttrjueqjojz.supabase.co",
+
+  SUPABASE_PUBLISHABLE_KEY:
+    "sb_publishable__dF8ANtuBA8bW4rZQ6lYXw_3N3Xzqkg",
+
+  ADMIN_API_URL:
+    "https://gfokwfsqsttrjueqjojz.supabase.co/functions/v1/ktms-admin-api",
+
+  APP_NAME: "KTMS Admin",
+
+  ROUTES: {
+    LOGIN: "/",
+    DASHBOARD: "/dashboard"
+  }
+};
