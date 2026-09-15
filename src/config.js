54fe8c13710d13b9b701d1faec3544834326c5ea
admin-ktms <admin.ktms@gmail.com>
2026-09-15 02:25:51 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -8,6 +8,9 @@ export const CONFIG = {
   ADMIN_API_URL:
     "https://gfokwfsqsttrjueqjojz.supabase.co/functions/v1/ktms-admin-api",
 
+  ADMIN_LOGIN_API_URL:
+  "https://gfokwfsqsttrjueqjojz.supabase.co/functions/v1/ktms-admin-login",
+
   APP_NAME: "KTMS Admin",
 
   ROUTES: {
